8640627930bb5034f46503634a4f2bcaa0a2ab2e
Jaspreet Singh <work.jaspreet1203@gmail.com>
2025-09-21 01:26:54 +0530

    UPDATING - learned till 404 page and redirection

--- react-404-page-and-redirection/src/App.jsx
+++ react-404-page-and-redirection/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Login from "./Login";
+import About from "./About";
+import PageNotFound from "./PageNotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <h1>404 Page and Redirection in React.js</h1> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/*" element={<PageNotFound />} /> */}
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
