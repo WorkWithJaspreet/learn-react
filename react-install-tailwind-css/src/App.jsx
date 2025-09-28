52c261012a06d536f788073e56cc3ed017ab1999
Jaspreet Singh <work.jaspreet1203@gmail.com>
2025-09-29 01:25:24 +0530

    UPDATING - learned to integrate tailwind css with react

---
+++ react-install-tailwind-css/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="block font-bold text-[32px] leading-tight my-[21.44px] mx-0 text-black font-[Times] [unicode-bidi:isolate]">
+        Integrating Tailwind CSS with React.js
+      </h1>
+    </>
+  );
+}
+
+export default App;
